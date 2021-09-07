3909cf14cdacbd29fc2355ac807050243b4c006a
Nhat Quan <quan.dn@geekup.vn>
2021-09-07 16:37:03 +0700

    add .env and change port

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 const queryString = require('query-string')
+const PORT = process.env.PORT || 3000;
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
 
@@ -52,6 +53,6 @@ router.render = (req, res) => {
 
 // Use default router
 server.use('/api',router)
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log('JSON Server is running')
 })
